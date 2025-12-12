8192314369274dfc38c69459cdb9c8de34266698
VidovicD <vidovicdanijel14@gmail.com>
2025-12-12 20:14:08 +0100

    Update

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -6,12 +6,6 @@ import ContactSection from "./ContactSection";
 import BlankGradient from "../../components/blank-gradient/BlankGradient";
 
 function HomePage() {
-  const zonesRef = useRef(null);
-
-  const scrollToZone = (zone) => {
-    zonesRef.current?.scrollToZone(zone);
-  };
-
   return (
     <>
       <HeroSection />
