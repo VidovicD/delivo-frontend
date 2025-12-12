49dbc4df1faa8563ea2418f0a0614a131bb564b6
VidovicD <vidovicdanijel14@gmail.com>
2025-12-12 21:40:44 +0100

    foodic

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import HeroSection from "./HeroSection";
 import ContactSection from "./ContactSection";
 
-import BlankGradient from "../../components/blank-gradient/BlankGradient";
 import FoodSection from "./FoodSection";
 
 function HomePage() {
