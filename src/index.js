876233db8244c43d040dac7dad7d6912d9fc1d06
VidovicD <vidovicdanijel14@gmail.com>
2025-12-14 00:54:51 +0100

    Laku noc...

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Delivo from "./Delivo";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <Delivo />
