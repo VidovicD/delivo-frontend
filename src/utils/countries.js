6e39180c36d0ff482a42c4484185574ffa72dca2
VidovicD <vidovicdanijel14@gmail.com>
2025-12-22 16:59:01 +0100

    gitara

---
+++ src/utils/countries.js
@@ -0,0 +1,23 @@
+export const COUNTRIES = [
+  {
+    code: "RS",
+    name: "Srbija",
+    dialCode: "381",
+    flag: "🇷🇸",
+    placeholder: "0641234567",
+  },
+  {
+    code: "HR",
+    name: "Hrvatska",
+    dialCode: "385",
+    flag: "🇭🇷",
+    placeholder: "0912345678",
+  },
+  {
+    code: "BA",
+    name: "Bosna i Hercegovina",
+    dialCode: "387",
+    flag: "🇧🇦",
+    placeholder: "061234567",
+  },
+];
